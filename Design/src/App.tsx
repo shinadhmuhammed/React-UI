d4534bec3415c7adb8e5503f239a8f2581580b24
muhammed shinadh <muhammedshinad2@gmail.com>
2024-09-03 23:26:21 +0530

    started the project by installing the packages like vite,shadcn ui,..

---
+++ Design/src/App.tsx
@@ -0,0 +1,28 @@
+// import UpcomingClasses from "./components/Classes/UpcomingClasses";
+
+function App() {
+  const classesData = [
+    {
+      name: "UI/UX Designing",
+      date: "Live (0:54)",
+      staffName: "Suriya R",
+      staffImage: "/path/to/suriya-image.jpg",
+      action: "Join now",
+    },
+    {
+      name: "Graphic Designing",
+      date: "Today 6pm",
+      staffName: "Priya Sweety",
+      staffImage: "/path/to/priya-image.jpg",
+      countdown: "2:40:34",
+    },
+  ];
+
+  return (
+    <>
+      {/* <UpcomingClasses classes={classesData} /> */}
+    </>
+  );
+}
+
+export default App;
---
+++ Design/src/components/Classes/UpcomingClasses.tsx
@@ -0,0 +1,28 @@
+// // import { Card,CardHeader, } from "../ui/card";
+
+
+
+// type UpcomingClassesProps = {
+//     classes: {
+//       name: string;
+//       date: string;
+//       staffName: string;
+//       staffImage: string;
+//       action?: string;
+//       countdown?: string;
+//       daysLeft?: number;
+//     }[];
+//   };
+
+
+// function UpcomingClasses({classes}:UpcomingClassesProps) {
+//   return (
+//     <div>
+    
+//     </div>
+//   )
+// }
+
+// export default UpcomingClasses
+
+
